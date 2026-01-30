deda63c48bea32d7f220e0570f207134c1963287
Yannis AULLEN CHOUBRAC <you@example.com>
2026-01-30 11:02:10 +0100

    sheep avance

--- Sheep.py
+++ Sheep.py
@@ -1,7 +1,7 @@
 import random
 
 class Sheep:
-    def __init__(self, x, y,
+    def __init__(self, x, y, grid_size,
                  SHEEP_INITIAL_ENERGY, 
                  SHEEP_ENERGY_LOSS_PER_TURN, 
                  SHEEP_ENERGY_FROM_GRASS, 
@@ -11,6 +11,7 @@ class Sheep:
                  ):
         self.x = x
         self.y = y
+        self.grid_size = grid_size
         self.energy = SHEEP_INITIAL_ENERGY
         self.age = 0
         self.energy_loss_per_turn = SHEEP_ENERGY_LOSS_PER_TURN
@@ -19,6 +20,13 @@ class Sheep:
         self.reproduction_energy_cost = REPRODUCTION_ENERGY_COST
         self.max_age = SHEEP_MAX_AGE
 
-    def move(self, grid_size):
+    def move(self):
+        x = self.x
+        y = self.y
+        grid_size = self.grid_size
         directions = [(0,1), (1,0), (0,-1), (-1,0)]
-        for 
+        for dx, dy in directions:
+            if 0 <= x + dx < grid_size and 0 <= y + dy < grid_size:  # on vérifie les limites de la grille
+                x += dx
+                y += dy
+                square_near = 
