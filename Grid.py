7bd9b5133ebaa87d05a69fb276fe0b70880e38c5
Lucie Dupouy <eva.luciedupouy@gmail.com>
2026-01-30 15:22:02 +0100

    après débugage

--- Grid.py
+++ Grid.py
@@ -11,10 +11,18 @@ class Grid :
     def __init__(self,GRID_SIZE,CUBE):
         self.GRID_SIZE=GRID_SIZE
         self.cube= CUBE
-        self.mouton_img = pg.image.load("mouton.png").convert_alpha()
-        self.mouton_img = pg.transform.scale(self.mouton_img,(self.cube, self.cube))
-        self.grid=np.array(self.GRID_SIZE,self.GRID_SIZE)
-        
+        self.wolf_list=[]
+        for _ in range(INITIAL_WOLVES):
+            x = randint(0, GRID_SIZE)
+            y = randint(0, GRID_SIZE)
+            self.wolf_list.append(Wolf(WOLF_INITIAL_ENERGY, x, y))
+        self.sheep_list=[]
+        for _ in range(INITIAL_SHEEP):
+            x = randint(0, GRID_SIZE)
+            y = randint(0, GRID_SIZE)
+            self.sheep_list.append(Sheep(SHEEP_INITIAL_ENERGY, x, y))
+        self.grid = np.empty((self.GRID_SIZE, self.GRID_SIZE), dtype=object)
+        self.vert=(20, 148, 20)
         for x in range(self.GRID_SIZE):
             for y in range(self.GRID_SIZE):
                 self.grid[x,y]=square
@@ -24,6 +32,10 @@ class Grid :
     def run(self):
         pg.init()
         self.screen = pg.display.set_mode((self.cube*self.GRID_SIZE, self.cube*self.GRID_SIZE))
+        self.loup_img = pg.image.load("loup.png").convert_alpha()
+        self.loup_img = pg.transform.scale(self.loup_img,(self.cube, self.cube))
+        self.mouton_img = pg.image.load("mouton.png").convert_alpha()
+        self.mouton_img = pg.transform.scale(self.mouton_img,(self.cube, self.cube))
 
         clock = pg.time.Clock()
 
@@ -50,8 +62,68 @@ class Grid :
             clock.tick(60)
 
         pg.quit()
+    
+    def affiche_herbe(self):
+        for x in range(self.GRID_SIZE):
+            for y in range(self.GRID_SIZE):
+                self.grid[x,y].growth()
+                if self.grid[x,y].grass :
+                    rect = pg.Rect(x*self.cube, y*self.cube, self.cube, self.cube)
+                    pg.draw.rect(self.screen, self.vert, rect)
 
-    def affichage_moutons(self,position_mouton):
+    def incrementer_age(self):
+        for mouton in self.sheep_list:
+            mouton.age+=1
+        for loup in self.wolf_list:
+            loup.age+=1
+    
+    def maj_animaux(self):
+        for mouton in self.sheep_list:
+            mouton.move_eat(self.grid)
+            mouton.energy-=SHEEP_ENERGY_LOSS_PER_TURN
+        for loup in self.wolf_list:
+            self.sheep_list=loup.move_eat(self.grid,self.sheep_list)
+            loup.energy-=WOLF_ENERGY_LOSS_PER_TURN
+    
+    def maj_mort(self): 
+        self.sheep_list = [m for m in self.sheep_list if m.energy > 0 and m.age < SHEEP_MAX_AGE]
+        self.wolf_list = [l for l in self.wolf_list if l.energy > 0 and l.age < SHEEP_MAX_AGE]
+    
+    
+    def affiche_animaux(self):
+        for mouton in self.sheep_list:
+            x,y=mouton.x, mouton.y
+            pixel_x = x * self.cube + self.cube // 2
+            pixel_y = y * self.cube + self.cube // 2
+            rect = self.mouton_img.get_rect(center=(pixel_x, pixel_y))
+            self.screen.blit(self.mouton_img, rect)
+        for loup in self.wolf_list:
+            x,y=loup.x, loup.y
+            pixel_x = x * self.cube + self.cube // 2
+            pixel_y = y * self.cube + self.cube // 2
+            rect = self.loup_img.get_rect(center=(pixel_x, pixel_y))
+            self.screen.blit(self.loup_img, rect)
+        
+    def reproduction(self):
+        for mouton in self.sheep_list:
+            mouton.reproduce()
+        for loup in self.wolf_list:
+            loup.reproduce()
+
+
+
+
+
+
+    
+
+
+
+    
+            
+        
+
+
 
 
 
