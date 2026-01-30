9bfe57f526845f9741d3a8ceaf92e0ee329a9386
Lucie Dupouy <eva.luciedupouy@gmail.com>
2026-01-30 11:34:28 +0100

    structure faite

--- Grid.py
+++ Grid.py
@@ -1,6 +1,10 @@
 from random import randint
 import pygame as pg
 from Ecosysteme import *
+import Sheep
+import Wolf 
+import Square
+import numpy as np 
 
 
 class Grid :
@@ -9,34 +13,45 @@ class Grid :
         self.cube= CUBE
         self.mouton_img = pg.image.load("mouton.png").convert_alpha()
         self.mouton_img = pg.transform.scale(self.mouton_img,(self.cube, self.cube))
+        self.grid=np.array(self.GRID_SIZE,self.GRID_SIZE)
+        for x in range(self.GRID_SIZE):
+            for y in range(self.GRID_SIZE):
+                self.grid[x,y]=square
+            
 
 
-    def trace_initial(self):
-        """
-        Affiche le terrain tout marron ( base du jeu )
-
-        """
+    def run(self):
         pg.init()
-        marron=(137, 81, 41 ) 
-        screen = pg.display.set_mode((self.cube*self.GRID_SIZE, self.cube*self.GRID_SIZE))
-        rect = pg.Rect(0,0 ,self.cube*self.GRID_SIZE, self.cube*self.GRID_SIZE)
-        pg.draw.rect(screen, marron , rect)
-        pg.display.update()
+        self.screen = pg.display.set_mode((self.cube*self.GRID_SIZE, self.cube*self.GRID_SIZE))
 
-        running = True
-        while running:
+        clock = pg.time.Clock()
+
+        while self.running:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    running = False
-        
-    
+                    self.running = False
 
-
-
+            
+            moutons = self.ecosysteme.positionsmoutons()
+            #à chaque boucle, on retrace tout
+            #écran marron
+            marron=(137, 81, 41)
+            self.screen.fill(marron)
+            #on dessine l'herbe
+            #array de booleen( true = herbe)
+            self.affiche_herbe(herbe)
+            #on dessine les moutons
+            self.affiche_moutons(moutons)
+            #on dessine les loups 
+            self.affiche_loups(loups)
+            #on met à jour
+            pg.display.update()
+            clock.tick(60)
 
         pg.quit()
 
     def affichage_moutons(self,position_mouton):
+
 
 
 
@@ -47,3 +62,5 @@ if __name__ == "__main__":
     CUBE = 30
     jeu=Grid(GRID_SIZE,CUBE)
     jeu.trace_initial()
+
+
